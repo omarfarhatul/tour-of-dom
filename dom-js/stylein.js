2820dcf72ba1da8fbfacb49329d88982bfe2a423
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-30 22:12:07 +0600

    Update

---
+++ dom-js/stylein.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border='2px solid steelblue';
+    section.style.marginBottom='5px';
+    section.style.borderRadius='10px';
+    section.style.padding='10px';
+    section.style.backgroundColor='lightgray';
+}
+
+// const placeContainer = document.getElementById('places-container');
+// placeContainer.style.backgroundColor='red';
